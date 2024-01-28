ab524d2594aee82c1008626bfc1be4bac6e2043c
aharting <74853627+aharting@users.noreply.github.com>
2024-01-28 13:24:05 +0100

    Update reproduce_results.sh

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
+
 # mcmc performance benchmark on simulated data
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/observable_dag_model/benchmark/
-python3 observable_dag_model_script.py
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/partially_observed_source_measurement_model/benchmark/
-python3 partially_observed_source_measurement_model_script.py
+cd "$SCRIPT_DIR/experiments/mcmc_performance/observable_dag_model/benchmark/" || exit
+python3 observable_dag_model_script.py || exit
+
+cd "$SCRIPT_DIR/experiments/mcmc_performance/partially_observed_source_measurement_model/benchmark/" || exit
+python3 partially_observed_source_measurement_model_script.py || exit
 
 # mcmc tuning on simulated data
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/observable_dag_model/tuning/initialization/
-python3 tuning_initialization_script.py
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/observable_dag_model/tuning/hyperparameters/
-python3 tuning_hyperparameters_script.py
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/partially_observed_source_measurement_model/tuning/initialization/
-python3 tuning_initialization_script.py
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/mcmc_performance/partially_observed_source_measurement_model/tuning/hyperparameters/
-python3 tuning_hyperparameters_script.py
+cd "$SCRIPT_DIR/experiments/mcmc_performance/observable_dag_model/tuning/initialization/" || exit
+python3 tuning_initialization_script.py || exit
+
+cd "$SCRIPT_DIR/experiments/mcmc_performance/observable_dag_model/tuning/hyperparameters/" || exit
+python3 tuning_hyperparameters_script.py || exit
+
+cd "$SCRIPT_DIR/experiments/mcmc_performance/partially_observed_source_measurement_model/tuning/initialization/" || exit
+python3 tuning_initialization_script.py || exit
+
+cd "$SCRIPT_DIR/experiments/mcmc_performance/partially_observed_source_measurement_model/tuning/hyperparameters/" || exit
+python3 tuning_hyperparameters_script.py || exit
 
 # ncfa on simulated data
-cd ~/mcmc-estimation-causal-vae-architectures/experiments/ncfa_with_mcmc/
-python3 ncfa_simulated_experiment_script.py
-
+cd "$SCRIPT_DIR/experiments/ncfa_with_mcmc/" || exit
+python3 ncfa_simulated_experiment_script.py || exit
